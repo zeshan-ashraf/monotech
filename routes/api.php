b05d06930759db2f8b4bebb3e75943ef94c426dd
root <root@srv893658.hstgr.cloud>
2025-07-08 17:19:19 +0000

    removed whitelist middleware

--- routes/api.php
+++ routes/api.php
@@ -40,13 +40,17 @@ Route::as('payin.')->prefix('payin')->group(function () {
 Route::post('v1/payin-checkout',[PaymentCheckoutController::class, 'checkoutProceed'])
     ->middleware(['payment.validate', 'check.blocked.numbers']);
     
-    
 Route::as('payout.')->prefix('payout')->group(function () {
+    // Route without whitelist.ip middleware
+    Route::post('/checkout', [PayoutController::class, 'checkout']);
+});
+    
+/*Route::as('payout.')->prefix('payout')->group(function () {
     Route::middleware('whitelist.ip')->group(function () {
         Route::post('/checkout',[PayoutController::class, 'checkout']);
     });
 });
-
+*/
 Route::post('/payin-status-check', [GeneralController::class , 'checkStatus']);
 Route::post('/payout-status-check', [GeneralController::class , 'checkPayoutStatus']);
 //Route::get('/get-dashboard-data', [GeneralController::class , 'dashboardData']);
