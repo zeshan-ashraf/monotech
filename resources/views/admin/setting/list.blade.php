6ff730245e8d1f8e6d969957b865de465a5a651c
Malik Nouman <noumansam120@gmail.com>
2025-07-13 15:37:45 +0500

    reverse list code updated

--- resources/views/admin/setting/list.blade.php
+++ resources/views/admin/setting/list.blade.php
@@ -7,7 +7,6 @@
 @section('content')
 <div class="app-content content ">
     <div class="content-overlay"></div>
-    <div class="header-navbar-shadow"></div>
     <div class="content-wrapper container-xxl pt-0 px-0 pb-sm-0 pb-5">
         <div class="content-header row">
         </div>
