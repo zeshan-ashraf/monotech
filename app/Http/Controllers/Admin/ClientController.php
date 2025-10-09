13c9154ef25808be7fcd58e33964619c2e2cb8c3
Malik Nouman <noumansam120@gmail.com>
2025-10-09 16:17:22 +0500

    add different jc and ep fee

--- app/Http/Controllers/Admin/ClientController.php
+++ app/Http/Controllers/Admin/ClientController.php
@@ -90,7 +90,7 @@ class ClientController extends Controller
         $request->validate([
             'payin_fee'=>'required',
             'payin_ep_fee'=>'required',
-            'payout_fee'=>'required',
+            'payout_fee'=>'required', 
             'per_payin_fee'=>'required',
             'per_payout_fee'=>'required',
         ]);    
