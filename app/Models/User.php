1d7e2818d186b74b0f0929aa357cf9595468541f
Malik Nouman <noumansam120@gmail.com>
2025-10-09 16:14:46 +0500

    add different jc and ep fee

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable
         'password',
         'visible_password',
         'payin_fee',
+        'payin_ep_fee',
         'payout_fee',
         'per_payin_fee',
         'per_payout_fee',
