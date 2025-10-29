3661ed8584bd28df0a43b6a639fdf23f7b8dcf0a
zeshan-ashraf <malikzeshanashraf@gmail.com>
2025-10-29 12:12:34 +0500

    unsettle is in -ve then prevent edit balance

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -262,26 +262,18 @@
                                                             @if(auth()->user()->user_role == "Super Admin" || auth()->user()->user_role == "Manager")
                                                             <td class="bg-warning">
                                                                 <div class="d-flex justify-content-start">
-                                                                    @if($item['unsettled_amount_balance'] < 0)
-                                                                        <button class="btn btn-danger w-auto" disabled 
-                                                                            title="Cannot add amount when balance is negative" 
-                                                                            style="cursor: not-allowed;">
-                                                                            <i class="fa fa-ban"></i>
-                                                                        </button>
+                                                                    @if($item['setting']->auto == 0)
+                                                                        <a class="dropdown-item btn btn-primary w-auto open_modal me-1" 
+                                                                           data-url="{{ route('admin.setting.modal') }}" 
+                                                                           data-id="{{ $user->id }}">
+                                                                            <i class="fa fa-edit"></i>
+                                                                        </a>
                                                                     @else
-                                                                        @if($item['setting']->auto == 0)
-                                                                            <a class="dropdown-item btn btn-primary w-auto open_modal me-1" 
-                                                                               data-url="{{ route('admin.setting.modal') }}" 
-                                                                               data-id="{{ $user->id }}">
-                                                                                <i class="fa fa-edit"></i>
-                                                                            </a>
-                                                                        @else
-                                                                            <a class="dropdown-item btn btn-success w-auto open_modal me-1" 
-                                                                               data-url="{{ route('admin.setting.third_modal') }}" 
-                                                                               data-id="{{ $user->id }}">
-                                                                                <i class="fa fa-edit"></i>
-                                                                            </a>
-                                                                        @endif
+                                                                        <a class="dropdown-item btn btn-success w-auto open_modal me-1" 
+                                                                           data-url="{{ route('admin.setting.third_modal') }}" 
+                                                                           data-id="{{ $user->id }}">
+                                                                            <i class="fa fa-edit"></i>
+                                                                        </a>
                                                                     @endif
                                                                 </div>
                                                             </td>
