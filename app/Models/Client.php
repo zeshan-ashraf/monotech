8fe9f23f82bbe6b01cf22fb9f5be8dff06fc5f21
Malik Nouman <noumansam120@gmail.com>
2026-04-20 16:34:02 +0500

    add new clients

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+
+    protected $fillable =[
+        'name',
+        'user_id',
+        'photo',
+    ];
 }
