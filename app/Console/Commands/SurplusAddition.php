159749aca0cbdfce92e39b788446c83ba423d8d5
Malik Nouman <noumansam120@gmail.com>
2026-06-11 15:23:57 +0500

    add user role and settings of new user

--- app/Console/Commands/SurplusAddition.php
+++ app/Console/Commands/SurplusAddition.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 use App\Models\{TempAmountPayout,Settlement,SurplusAmount};
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 
 class SurplusAddition extends Command
 {
@@ -31,29 +32,44 @@ class SurplusAddition extends Command
      */
     public function handle()
     {
-        $tempAmount=TempAmountPayout::first();
-        $surplus=SurplusAmount::first();
-        $totals = Settlement::whereDate('date', today())
-            ->selectRaw('
-                COALESCE(SUM(jc_payout), 0) as payoutSumJC,
-                COALESCE(SUM(ep_payout), 0) as payoutSumEP,
-                COALESCE(SUM(ibft_amount), 0) as ibftAmount
-            ')
-            ->first();
+        try {
+            DB::beginTransaction();
 
-        $totalJCPayout = $totals->payoutSumJC + $totals->ibftAmount;
-        $totalEPPayout = $totals->payoutSumEP;
+            $tempAmount = TempAmountPayout::first();
+            $surplus = SurplusAmount::first();
 
-        $surplus->update([
-            'jazzcash' => $surplus->jazzcash + $tempAmount->jc_amount - $totalJCPayout,
-            'easypaisa' => $surplus->easypaisa + $tempAmount->ep_amount - $totalEPPayout,
-        ]);
+            $totals = Settlement::whereDate('date', today())
+                ->selectRaw('
+                    COALESCE(SUM(jc_payout), 0) as payoutSumJC,
+                    COALESCE(SUM(ep_payout), 0) as payoutSumEP,
+                    COALESCE(SUM(ibft_amount), 0) as ibftAmount
+                ')
+                ->first();
 
-        $tempAmount->update([
-            'jc_amount' => $totalJCPayout,
-            'ep_amount' => $totalEPPayout,
-        ]);
-                    
-        $this->info('Adding surplus amount in wallet successfully.');
+            $totalJCPayout = $totals->payoutSumJC + $totals->ibftAmount;
+            $totalEPPayout = $totals->payoutSumEP;
+
+            $surplus->update([
+                'jazzcash' => $surplus->jazzcash + $tempAmount->jc_amount - $totalJCPayout,
+                'easypaisa' => $surplus->easypaisa + $tempAmount->ep_amount - $totalEPPayout,
+            ]);
+
+            $tempAmount->update([
+                'jc_amount' => $totalJCPayout,
+                'ep_amount' => $totalEPPayout,
+            ]);
+
+            DB::commit();
+
+        } catch (\Throwable $e) {
+
+            DB::rollBack();
+
+            Log::channel('daily')->error('Settlement Cron Error', [
+                'message' => $e->getMessage(),
+                'line' => $e->getLine(),
+                'file' => $e->getFile(),
+            ]);
+        }
     }
 }
