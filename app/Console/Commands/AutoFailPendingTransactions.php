f7cbe3c7778d866d64e89821b3be178c4d6be86c
Malik Nouman <noumansam120@gmail.com>
2026-06-20 21:39:27 +0500

    create single wallet in dashbaord api

--- app/Console/Commands/AutoFailPendingTransactions.php
+++ app/Console/Commands/AutoFailPendingTransactions.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Models\Transaction;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class AutoFailPendingTransactions extends Command
 {
@@ -22,6 +23,12 @@ class AutoFailPendingTransactions extends Command
                 'pp_code' => '999',
                 'pp_message' => 'Auto-failed after 60 minutes'
             ]);
+
+        DB::table('backup_transactions')
+            ->where('created_at', '<', now()->subMonths(2))
+            ->limit(1000)
+            ->delete();
+
 
         $this->info("Updated $count transaction(s) to failed.");
     }
