e0ade9cb365d1af0a3836fe484d96dc82f153e6b
zeshan-ashraf <malikzeshanashraf@gmail.com>
2026-08-22 17:12:12 +0500

    status callback payin

--- resources/views/admin/transaction/callback_badge.blade.php
+++ resources/views/admin/transaction/callback_badge.blade.php
@@ -1,5 +1,6 @@
 @php
-    $sent = (int) ($callbackSent ?? 0) === 1;
+    $sentOut = ((int) ($callbackSent ?? 0) === 1) || !empty($callbackSentAt);
+    $gotReply = !empty($callbackResponseAt);
     $reply = trim((string) ($callbackResponse ?? ''));
     $sentAt = \App\Support\PayinCallbackTracker::formatTimestamp($callbackSentAt ?? null);
     $responseAt = \App\Support\PayinCallbackTracker::formatTimestamp($callbackResponseAt ?? null);
@@ -14,10 +15,22 @@
     $tip = implode('<br>', array_map('e', $tipParts));
 @endphp
 <span class="status-badge-wrap">
-    <span class="badge {{ $sent ? 'bg-success' : 'bg-secondary' }}"
-          data-bs-toggle="tooltip"
-          data-bs-html="true"
-          title="{!! $tip !!}">
-        {{ $sent ? 'Sent' : 'Not Sent' }}
-    </span>
+    @if($sentOut && $gotReply)
+        <i class="fas fa-check-double text-success font-medium-3"
+           data-bs-toggle="tooltip"
+           data-bs-html="true"
+           title="{!! $tip !!}"
+           aria-label="Callback sent and reply received"></i>
+    @elseif($sentOut)
+        <i class="fas fa-check text-success font-medium-3"
+           data-bs-toggle="tooltip"
+           data-bs-html="true"
+           title="{!! $tip !!}"
+           aria-label="Callback sent, waiting for reply"></i>
+    @else
+        <span class="text-muted"
+              data-bs-toggle="tooltip"
+              data-bs-html="true"
+              title="{!! $tip !!}">Not Sent</span>
+    @endif
 </span>
