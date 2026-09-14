65ba71100b1e55984acc2d0fa87d0896e3894df4
zeshan-ashraf <malikzeshanashraf@gmail.com>
2026-09-14 13:30:41 +0500

    dashboard adjustmen. dashboar client order metrics

--- app/Services/SettlementDashboardService.php
+++ app/Services/SettlementDashboardService.php
@@ -101,6 +101,9 @@ class SettlementDashboardService
         $clients = User::query()
             ->where('user_role', 'Client')
             ->where('active', 1)
+            ->orderByRaw('db_metrics_order IS NULL')
+            ->orderBy('db_metrics_order')
+            ->orderBy('name')
             ->get();
 
         $clientIds = $clients->pluck('id')->all();
@@ -208,10 +211,7 @@ class SettlementDashboardService
             $totals['total_ibft_amount'] += $item['ibft_amount'] ?? 0;
         }
 
-        $viewRows = collect($data)
-            ->sortBy(fn ($item) => $item['user']->id == 24 ? 1 : 0)
-            ->values()
-            ->all();
+        $viewRows = $data;
 
         $pollRows = collect($viewRows)
             ->filter(function ($item) use ($viewer) {
